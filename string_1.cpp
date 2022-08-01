eaefd24302d9a5edc1845d2e2a45304812c22152
unknown <abduhamidovm2007@gmail.com>
2022-08-01 10:40:42 +0500

    1-loyiha

---
+++ string_1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	
+// assalomualekom
+	int n,k,i;
+	string s,c="";
+	cin>>s;
+	n=s.size();
+	for (i=0;i<=n-1;i++)
+	{
+		if(i>=2 and i<=6)
+		{
+			c=c+s[i];
+		}
+	}
+	cout<<c;	
+//	
+//==========================================================
+
+	{
+	
+		//assalomualekom
+//		int n,k,i;
+//		string s,c="";
+//		cin>>s;
+//		n=s.size();
+//		for (i=0;i<=n-1;i++)
+//		{
+//			if(not(i>=2 and i<=6))
+//			{
+//			c=c+s[i];
+//			}
+//		}
+//		cout<<c;	
+//	}
+	
+}
+}
